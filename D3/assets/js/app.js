afc0747b1d311e8374f1255b4cb256e12e26b4b0
vaideheeshah13 <79955097+vaideheeshah13@users.noreply.github.com>
2021-07-29 20:25:29 -0500

    Add files via upload

---
+++ D3/assets/js/app.js
@@ -0,0 +1,89 @@
+// This is correct one
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("../data/data.csv").then(function(chartData) {
+
+    chartData.forEach(function(data) {
+      data.healthcare = +data.healthcare;
+      data.income = +data.income;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(chartData, d => d.income)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([35000, d3.max(chartData, d => d.obesity)])
+      .range([height, 0]);
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(chartData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.income))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "15")
+    .attr("fill", "lightblue")
+    .attr("opacity", ".5");
+
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>Income: ${d.income}%<br>Obesity: $${d.obesity}`);
+      });
+
+    chartGroup.call(toolTip);
+
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 30)
+      .attr("x", 0 - (height / 1.5))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Obesity Level (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width/3}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Median Household Income ($)");
+  }).catch(function(error) {
+    console.log(error);
+  });
